6b4fdbec0dbfcdca79983a37937448a85abb26eb
Harry Horton <n0vat3k@gmail.com>
2019-03-30 17:36:31 -0400

    add apollo

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ApolloExample/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ApolloExample from "@/components/ApolloExample.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    ApolloExample
   }
 };
 </script>
